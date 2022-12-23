ed53c72479c4b2279a66532657f8f5dab513564f
Ishlahul Hanif <m.ishlahulh@gmail.com>
2022-12-23 13:20:40 +0700

    Initial commit

---
+++ src/main/kotlin/shlh/aztech/Avalon/AvalonApplication.kt
@@ -0,0 +1,11 @@
+package shlh.aztech.Avalon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AvalonApplication
+
+fun main(args: Array<String>) {
+	runApplication<AvalonApplication>(*args)
+}
